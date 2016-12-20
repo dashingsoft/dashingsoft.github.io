3a3dc2bf3243eb9c3bb656ba87e040bb4e8ec25c
Jondy Zhao <jondy.zhao@gmail.com>
2016-12-20 16:00:41 +0800

    Add pyarmor 2.5.3

---
+++ pyarmor/update.sh
@@ -0,0 +1,5 @@
+scp jondy@192.168.121.103:/home/jondy/workspace/pyarmor-docs/_build/html/* .
+scp -r jondy@192.168.121.103:/home/jondy/workspace/pyarmor-docs/_build/html/_static .
+scp -r jondy@192.168.121.103:/home/jondy/workspace/pyarmor-docs/_build/html/_sources .
+
+
