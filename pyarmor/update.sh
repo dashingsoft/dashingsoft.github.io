a7885d73981fdcec78d541445b9156c2ee11b8e1
Jondy Zhao <jondy.zhao@gmail.com>
2017-09-02 14:23:16 +0800

    Refresh pyarmor

--- pyarmor/update.sh
+++
@@ -1,5 +0,0 @@
-scp jondy@192.168.121.103:/home/jondy/workspace/pyarmor-docs/_build/html/* .
-scp -r jondy@192.168.121.103:/home/jondy/workspace/pyarmor-docs/_build/html/_static .
-scp -r jondy@192.168.121.103:/home/jondy/workspace/pyarmor-docs/_build/html/_sources .
-
-
