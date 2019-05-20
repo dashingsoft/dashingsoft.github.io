96e368da3fc3a687db598bfbcbe6a5a539b9b27a
Jondy Zhao <jondy.zhao@gmail.com>
2019-05-20 14:36:54 +0800

    Add prev/next button for customer list

--- www/js/menu.js
+++ www/js/menu.js
@@ -6,3 +6,11 @@ document.querySelector('button.navbar-toggler').addEventListener('click', functi
 document.addEventListener('click', function (e) {
     document.querySelector('div#navbar-main-tab').classList.remove('show');
 });
+
+document.querySelector('.carousel-control-prev').addEventListener('click', function (e) {
+    e.currentTarget.parentElement.firstElementChild.scrollLeft += 100;
+});
+
+document.querySelector('.carousel-control-next').addEventListener('click', function (e) {
+    e.currentTarget.parentElement.firstElementChild.scrollLeft -= 100;
+});
