6c08269ad8c10a18646d788ad86ad211274c2f94
Jondy Zhao <jondy.zhao@gmail.com>
2019-07-14 19:50:38 +0800

    Fix bug

--- www/js/menu.js
+++ www/js/menu.js
@@ -7,10 +7,12 @@ document.addEventListener('click', function (e) {
     document.querySelector('div#navbar-main-tab').classList.remove('show');
 });
 
-document.querySelector('.carousel-control-prev').addEventListener('click', function (e) {
-    e.currentTarget.parentElement.firstElementChild.scrollLeft += 100;
-});
+if (document.querySelector('.carousel-control-prev')) {
+    document.querySelector('.carousel-control-prev').addEventListener('click', function (e) {
+        e.currentTarget.parentElement.firstElementChild.scrollLeft += 100;
+    });
 
-document.querySelector('.carousel-control-next').addEventListener('click', function (e) {
-    e.currentTarget.parentElement.firstElementChild.scrollLeft -= 100;
-});
+    document.querySelector('.carousel-control-next').addEventListener('click', function (e) {
+        e.currentTarget.parentElement.firstElementChild.scrollLeft -= 100;
+    });
+}
