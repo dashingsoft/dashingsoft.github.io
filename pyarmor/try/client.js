7e95e405a4eef4549ec5f9549fd17c14d9bfd0c9
Jondy Zhao <jondy.zhao@gmail.com>
2025-03-01 17:01:39 +0800

    Refine try feature

--- pyarmor/try/client.js
+++ pyarmor/try/client.js
@@ -11,10 +11,9 @@ const on_start_build = () => {
     resultModal.querySelector('.modal-body > div.alert:nth-child(1)').classList.remove('d-none')
     resultModal.querySelector('.modal-body > div.alert:nth-child(2)').classList.add('d-none')
     resultModal.querySelector('.modal-body > div.alert:nth-child(3)').classList.add('d-none')
-    resultModal.querySelector('.modal-footer > button:nth-child(2)').classList.add('d-none')
 
     taskId = 0
-    taskStatus = 2
+    taskStatus = 0
     elapseSeconds = 0
     count_time()
 }
@@ -27,38 +26,33 @@ const on_server_error = (errmsg) => {
 }
 
 const start_build = (target) => {
-    const url = webapi
     const scriptName = document.getElementById('scriptName').value
     const pyver = document.getElementById('pythonVersion').value
     const content = document.getElementById('scriptContent').value
 
-    const data = {
+    const paras = {
         script: scriptName,
         pyver: pyver,
-        source: content,
         target: target,
     }
+    const url = make_url(webapi, paras)
 
     const onsuccess = (task) => {
         taskId = task.id
-        taskStatus = task.status
-        if (taskStatus == 2)
-            on_start_build()
-        else
-            on_task_error('${taskStatus}')
+        taskStatus = task.state
+        if (taskStatus >= 10) {
+            query_task()
+        }
     }
 
-    send_request('GET', url, data, onsuccess, on_task_error)
+    send_request('POST', url, content, onsuccess, on_task_error)
 }
 
-const make_url = (method, url, data) => {
-    if (method === "GET" && data) {
-        const paras = []
-        for (const prop in data)
-            paras.push(`${prop}=${data[prop]}`)
-        return `${url}?${paras.join('&')}`
-    }
-    return url
+const make_url = (url, data) => {
+    const paras = []
+    for (const prop in data)
+        paras.push(`${prop}=${data[prop]}`)
+    return `${url}?${paras.join('&')}`
 }
 
 const on_task_error = (errmsg) => {
@@ -68,17 +62,18 @@ const on_task_error = (errmsg) => {
 
 const send_request = (method, url, data, onsuccess, onerror) => {
 
-    const req = new Request(make_url(method, url, data))
+    const req = new Request(url)
     const headers = new Headers()
 
     // 只是为了开发测试使用, admin 用户和密码
     // 'WWW-Authenticate': 'Basic YWRtaW46YWRtaW4='
+    headers.append('Authorization', 'Basic YWRtaW46YWRtaW4=')
 
     // flyuser 用户的密码 "(thxjv!t8-F9*9j!q=)"
-    const username = `flyuser-pyarmor`
-    const basicauth = btoa(`${username}:(thxjv!t8-F9*9j!q=)`)
+    // const username = `flyuser-pyarmor`
+    // const basicauth = btoa(`${username}:(thxjv!t8-F9*9j!q=)`)
     // headers.append('Authorization', `Basic ${basicauth}`)
-    headers.append('Authorization', 'Basic YWRtaW46YWRtaW4=')
+    // headers.append("Content-Type", "application/octet-stream")
 
     if (method === 'POST' || method === 'PUT')
         headers.append("Content-Type", "application/json")
@@ -98,93 +93,62 @@ const send_request = (method, url, data, onsuccess, onerror) => {
         })
 
         .then((data) => {
-            onsuccess(data)
+            if (data.err) {
+                throw new Error(data.errmsg)
+            }
+            else
+                onsuccess(data)
         })
 
         .catch((err) => {
-            console.log(err)
             onerror(err)
         })
 }
 
-
-const request_download = (onsuccess, onerror) => {
-    const headers = new Headers()
-    const username = `flyuser-pyarmor`
-    const basicauth = btoa(`${username}:(thxjv!t8-F9*9j!q=)`)
-    headers.append('Authorization', `Basic ${basicauth}`)
-    headers.append("Content-Type", "application/json")
-
-    const url = webapi
-    const body = JSON.stringify({
-        pk: taskId,
-        action: "download",
-    })
-    const method = "POsT"
-    const options = {
-        mode: "cors",
-        headers: headers,
-        method: method,
-        body: body,
-    }
-
-    fetch(url, options)
-
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`${response.status}`)
-            }
-            return response.blob()
-        })
-
-        .then((response) => {
-            const href = URL.createObjectURL(response)
-            onsuccess(href)
-        })
-
-        .catch((err) => onerror(err))
-}
-
-const download_script = (script) => {
-    const onsuccess = (href) => {
-        const a = document.createElement('a')
-        a.href = href
-        a.setAttribute('download', `${script}`)
-        a.click()
-        URL.revokeObjectURL(href)
-    }
-    request_download(onsuccess, on_task_error)
+const enable_download_link = (data) => {
+    const scriptName = document.getElementById('scriptName').value
+    const blob = new Blob([data])
+    const a = resultModal.querySelector('.modal-body > div.alert:nth-child(3) > a')
+    a.href = URL.createObjectURL(blob)
+    a.download = scriptName
+    resultModal.querySelector('.modal-body > div.alert:nth-child(1)').classList.add('d-none')
+    resultModal.querySelector('.modal-body > div.alert:nth-child(2)').classList.add('d-none')
+    resultModal.querySelector('.modal-body > div.alert:nth-child(3)').classList.remove('d-none')
+    console.log(data)
 }
 
 const query_task = () => {
-    const url = webapi
-    const data = {
-        pk: taskId,
-        action: 'query',
+    if (taskId === 0)
+        return
+
+    const paras = {
+        pk: taskId
     }
+    const url = make_url(webapi, paras)
 
     const onsuccess = (task) => {
         taskId = task.id
-        taskStatus = task.status
+        taskStatus = task.state
         if (taskStatus == 10) {
-            resultModal.querySelector('.modal-footer > button:nth-child(3)').classList.remove('d-none')
+            enable_download_link(task.data)
         }
         else if (taskStatus > 10) {
-            on_task_error('${taskStatus}')
+            on_task_error(`${taskStatus}`)
         }
-        else if (taskStatus != 2) {
-            on_task_error('${taskStatus}')
+        else if (taskStatus != 2 && taskStatus != 0) {
+            on_task_error(`${taskStatus}`)
         }
     }
 
-    send_request('GET', url, data, onsuccess, on_task_error)
+    send_request('GET', url, null, onsuccess, on_task_error)
 }
 
 const count_time = () => {
     elapseSeconds += 1
     document.getElementById('worktime').innerHTML = `${elapseSeconds}`
-    if (taskStatus == 2) {
-        query_task()
+    if (taskStatus < 10) {
+        if (elapseSeconds % 5 === 0)
+            query_task()
         setTimeout(count_time, 1000)
     }
 }
@@ -195,6 +159,7 @@ if (resultModal) {
         const button = event.relatedTarget
         // Extract info from data-bs-* attributes
         const target = button.getAttribute('data-bs-whatever')
+        on_start_build()
         start_build(target)
     })
 
@@ -203,10 +168,6 @@ if (resultModal) {
             const modal = new bootstrap.Modal(confirmModal)
             modal.show()
         }
-    })
-
-    resultModal.querySelector('.modal-footer > button:nth-child(2)').addEventListener('click', event => {
-        download_script(document.getElementById('scriptName').value)
     })
 }
 
