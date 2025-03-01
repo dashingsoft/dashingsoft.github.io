2e5e5972a2383289aa976f5db6e1901f4878d533
Jondy Zhao <jondy.zhao@gmail.com>
2025-03-01 20:10:41 +0800

    Refine code

--- pyarmor/try/client.js
+++ pyarmor/try/client.js
@@ -2,7 +2,7 @@ let taskId = 0
 let taskStatus = 0
 let elapseSeconds = 0
 
-const webapi = 'http://192.168.121.108:8000/api/r1/tasks/try_pyarmor/'
+const webapi = '/api/r1/tasks/try_pyarmor/'
 
 const resultModal = document.getElementById('resultModal')
 const confirmModal = document.getElementById('confirmModal')
@@ -71,14 +71,15 @@ const send_request = (method, url, data, onsuccess, onerror) => {
 
     // 只是为了开发测试使用, admin 用户和密码
     // 'WWW-Authenticate': 'Basic YWRtaW46YWRtaW4='
-    headers.append('Authorization', 'Basic YWRtaW46YWRtaW4=')
+    // headers.append('Authorization', 'Basic YWRtaW46YWRtaW4=')
 
     // flyuser 用户的密码 "(thxjv!t8-F9*9j!q=)"
-    // const username = `flyuser-pyarmor`
-    // const basicauth = btoa(`${username}:(thxjv!t8-F9*9j!q=)`)
-    // headers.append('Authorization', `Basic ${basicauth}`)
-    // headers.append("Content-Type", "application/octet-stream")
+    const username = `flyuser-pyarmor`
+    const basicauth = btoa(`${username}:(thxjv!t8-F9*9j!q=)`)
+    headers.append('Authorization', `Basic ${basicauth}`)
 
+
+    // headers.append("Content-Type", "application/octet-stream")
     if (method === 'POST' || method === 'PUT')
         headers.append("Content-Type", "application/json")
 
@@ -112,7 +113,7 @@ const send_request = (method, url, data, onsuccess, onerror) => {
 const enable_download_link = (data) => {
     const scriptName = document.getElementById('scriptName').value
     const blob = new Blob([data])
-    const a = resultModal.querySelector('.modal-body > div.alert:nth-child(3) > a')
+    const a = resultModal.querySelector('.modal-body > div.alert:nth-child(3) > p > a')
     a.href = URL.createObjectURL(blob)
     a.download = scriptName
     resultModal.querySelector('.modal-body > div.alert:nth-child(1)').classList.add('d-none')
